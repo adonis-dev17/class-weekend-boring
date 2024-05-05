ccbf4a43c6edc7329bd3475afbd39184d1fbe460
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-06 01:16:36 +0800

    Add conditionals.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@ document.querySelector('#check').addEventListener('click', check)
 function check() {
 
   const day = document.querySelector('#day').value
-
+  let dayLowerCase = day.toLowerCase();
   //Conditionals go here
-
-
+ 
+if (dayLowerCase === 'tuesday' || dayLowerCase === 'thursday') {
+  document.querySelector('#placeToSee').innerHTML = 'You have a class!';
+} else if (dayLowerCase === 'saturday' || dayLowerCase === 'sunday') {
+  document.querySelector('#placeToSee').innerHTML = 'It is weekend!';
+} else {
+  document.querySelector('#placeToSee').innerHTML = 'No class, BOOOring';
 }
+ 
+}
+
